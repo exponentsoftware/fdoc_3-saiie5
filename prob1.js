5ad2a432f232fd414f26bbbae4c6a858f4bffd04
saiie5 <56484744+saiie5@users.noreply.github.com>
2021-08-31 13:23:22 +0530

    prob1 completed

---
+++ prob1.js
@@ -0,0 +1,6 @@
+  
+const student = ['David', ['HTM', 'CSS', 'JS', 'React'], [98, 85, 90, 95]]
+let [name, skills,scores]=student
+let [html, css, jsScore,reactScore]=scores
+console.log(name, skills, scores)
+console.log(jsScore, reactScore)
